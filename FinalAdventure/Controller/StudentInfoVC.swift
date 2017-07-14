7f9e45c353e380b5a57aab5665fea463fdd85bbf
Jack Korbin Kelley <jack@iMac.local>
2017-07-14 10:35:05 +0800

    sign in UI finished. Works like a charm. segue #1 and #2 working

---
+++ FinalAdventure/Controller/StudentInfoVC.swift
@@ -0,0 +1,21 @@
+//
+//  StudentInfoVC.swift
+//  FinalAdventure
+//
+//  Created by Jack Korbin Kelley on 14/7/2017.
+//  Copyright © 2017 Jack Korbin Kelley. All rights reserved.
+//
+
+import UIKit
+
+class StudentInfoVC: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
